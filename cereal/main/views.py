eb9c208a829f9eb9fd22f8ff28efa6428caaead8
Peter Henry <phenry@caltech.edu>
2015-07-10 20:32:43 -0600

    tightend up spacing in manufacturers view

--- cereal/main/views.py
+++ cereal/main/views.py
@@ -5,11 +5,8 @@ from django.http import HttpResponse
 
 
 def manufacturers(request):
-
     manufacturers = Manufacturer.objects.all()
     response = ['<h1>Cereal Manufacturers:</h1><br>']
-
     for manufacturer in manufacturers:
         response.append('%s<br>' % manufacturer.name)
-
     return HttpResponse(response)
