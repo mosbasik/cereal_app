db28cdf0bbb9f156e16110956eb80789258c96d3
Peter Henry <phenry@caltech.edu>
2015-07-10 20:28:49 -0600

    created "manufacturers/" view

--- cereal/main/views.py
+++ cereal/main/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from main.models import Manufacturer
 
-# Create your views here.
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def manufacturers(request):
+
+    manufacturers = Manufacturer.objects.all()
+    response = ['<h1>Cereal Manufacturers:</h1><br>']
+
+    for manufacturer in manufacturers:
+        response.append('%s<br>' % manufacturer.name)
+
+    return HttpResponse(response)
