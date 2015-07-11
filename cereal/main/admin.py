2e8c4bc897c5d8d086fa975ab079b02c8b827f6c
Peter Henry <phenry@caltech.edu>
2015-07-10 19:51:33 -0600

    removed wip comments from admin.py

--- cereal/main/admin.py
+++ cereal/main/admin.py
@@ -20,23 +20,6 @@ class CerealAdmin(admin.ModelAdmin):
                     'cup',
                     'rating',)
 
-    # name = models.CharField(max_length=100, null=True, blank=True)
-    # manufacturer = models.ForeignKey('main.Manufacturer')
-    # kind = models.ForeignKey('main.Kind')
-    # calorie = models.IntegerField(null=True, blank=True)
-    # protein = models.IntegerField(null=True, blank=True)
-    # fat = models.IntegerField(null=True, blank=True)
-    # sodium = models.IntegerField(null=True, blank=True)
-    # fiber = models.FloatField(null=True, blank=True)
-    # carb = models.FloatField(null=True, blank=True)
-    # sugar = models.IntegerField(null=True, blank=True)
-    # potassium = models.IntegerField(null=True, blank=True)
-    # vitamin = models.IntegerField(null=True, blank=True)
-    # shelf = models.ForeignKey('main.Shelf')
-    # weight = models.FloatField(null=True, blank=True)
-    # cup = models.FloatField(null=True, blank=True)
-    # rating = models.FloatField(null=True, blank=True)
-
 
 class ManufacturerAdmin(admin.ModelAdmin):
     list_display = ('name', 'abbreviation', )
